504b128272952aa2528e36d84165057b1e58776d
Ubuntu <ubuntu@ip-172-31-43-74.us-east-2.compute.internal>
2020-05-11 18:35:39 +0000

    Mesaj

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.use('/', express.static('frontend'))
-app.listen
+app.listen(process.env.PORT||8080)
 
 const Sequelize = require('sequelize')
 
